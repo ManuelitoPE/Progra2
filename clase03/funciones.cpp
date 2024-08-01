c33ab05f285121533ae1fbad282ae4327813d2ed
MAKO <a20210646@pucp.edu.pe>
2024-08-01 14:58:07 -0500

    clase03

---
+++ clase03/funciones.cpp
@@ -0,0 +1,18 @@
+//Punteros MULTIPLES
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+    
+    char nombre[100]; //arreglo estatico
+    char *ptr_nombre; //puntero dinamico
+
+    ptr_nombre = new char[100];
+
+    char ***productos;
+    int *srock;
+    double *precio;
+    q
+
+    return 0;
+}
