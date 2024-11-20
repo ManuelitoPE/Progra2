f31dd4757cd0782268a38a68e6ba0e75cdf2ae45
Sergio Sutta <101160113+ManuelitoPE@users.noreply.github.com>
2024-11-19 19:10:24 -0500

    Update Novela.cpp

--- 2024-1/labs/lab08/Novela.cpp
+++ 2024-1/labs/lab08/Novela.cpp
@@ -1,2 +1,26 @@
 //Author: MAKO
 #include "header/Novela.h"
+
+Novela::Novela() {
+    autor = {};
+}
+
+Novela::Novela(const Novela& orig):Libro(orig) {
+    *this = orig;
+}
+
+Novela::~Novela() {
+    delete[] autor;
+}
+
+void Novela::SetAutor(const char* autor) {
+    if(this->autor != nullptr)delete[] this->autor;
+    this->autor = new char[strlen(autor)+1]{};
+    strcpy(this->autor,autor);
+}
+
+void Novela::GetAutor(char* autor) const {
+    if(autor != nullptr)
+        strcpy(autor,this->autor);
+}
+
