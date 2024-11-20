a0a809cac73ec9550dd242b5f48e563ebaa4219d
Sergio Sutta <101160113+ManuelitoPE@users.noreply.github.com>
2024-11-19 19:13:31 -0500

    Update Libro.h

--- 2024-1/labs/lab08/header/Libro.h
+++ 2024-1/labs/lab08/header/Libro.h
@@ -1,21 +1,30 @@
 #ifndef LIBRO_H
 #define LIBRO_H
 
-class Libro{
-private:
-    
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+class Libro {
 public:
-    Libro(/* args */);
-    ~Libro();
+    Libro();
+    Libro(const Libro& orig);
+    virtual ~Libro();
+    void SetPeso(const double peso);
+    double GetPeso() const;
+    void SetPaginas(const int paginas);
+    int GetPaginas() const;
+    void SetNombre(const char* nombre);
+    void GetNombre(char* nombre) const;
+    void operator=(const Libro& libro);
+
+private:
+    char* nombre;
+    int paginas;
+    double peso;
 };
 
-Libro::Libro(/* args */)
-{
-}
+#endif /* LIBRO_H */
 
-Libro::~Libro()
-{
-}
-
-
-#endif
