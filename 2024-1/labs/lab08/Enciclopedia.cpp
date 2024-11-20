b21ae53a9e61b2bb1e9b91b32ecf170f0b50e1f8
Sergio Sutta <101160113+ManuelitoPE@users.noreply.github.com>
2024-11-19 19:10:48 -0500

    Update Enciclopedia.cpp

--- 2024-1/labs/lab08/Enciclopedia.cpp
+++ 2024-1/labs/lab08/Enciclopedia.cpp
@@ -1,2 +1,40 @@
 //Author: MAKO
 #include "header/Enciclopedia.h"
+
+Enciclopedia::Enciclopedia() {
+    sku = {};
+    anho = {};
+    vigencia = 1;
+}
+
+Enciclopedia::Enciclopedia(const Enciclopedia& orig):Libro(orig) {
+    *this = orig;
+}
+
+Enciclopedia::~Enciclopedia() {
+}
+
+void Enciclopedia::SetVigencia(int vigencia) {
+    this->vigencia = vigencia;
+}
+
+int Enciclopedia::GetVigencia() const {
+    return vigencia;
+}
+
+void Enciclopedia::SetAnho(int anho) {
+    this->anho = anho;
+}
+
+int Enciclopedia::GetAnho() const {
+    return anho;
+}
+
+void Enciclopedia::SetSku(int sku) {
+    this->sku = sku;
+}
+
+int Enciclopedia::GetSku() const {
+    return sku;
+}
+
