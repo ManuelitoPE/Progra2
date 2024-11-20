a2fe7ba4beaeab712a3750db983a5bd49a4757d4
Sergio Sutta <101160113+ManuelitoPE@users.noreply.github.com>
2024-11-19 19:05:11 -0500

    Update Revista.cpp

--- 2024-1/labs/lab08/Revista.cpp
+++ 2024-1/labs/lab08/Revista.cpp
@@ -1,2 +1,49 @@
 //Author: MAKO
 #include "header/Revista.h" 
+
+Revista::Revista() {
+    ISSN = {};
+    anho = {};
+    numero = {};
+    vigencia = 1;
+}
+
+Revista::Revista(const Revista& orig):Libro(orig){
+    *this = orig;
+}
+
+Revista::~Revista() {
+}
+
+void Revista::SetVigencia(int vigencia) {
+    this->vigencia = vigencia;
+}
+
+int Revista::GetVigencia() const {
+    return vigencia;
+}
+
+void Revista::SetNumero(int numero) {
+    this->numero = numero;
+}
+
+int Revista::GetNumero() const {
+    return numero;
+}
+
+void Revista::SetAnho(int anho) {
+    this->anho = anho;
+}
+
+int Revista::GetAnho() const {
+    return anho;
+}
+
+void Revista::SetISSN(int ISSN) {
+    this->ISSN = ISSN;
+}
+
+int Revista::GetISSN() const {
+    return ISSN;
+}
+
