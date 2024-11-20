1d733bca9035a35178ac36a65f1edab04b38a9dd
Sergio Sutta <101160113+ManuelitoPE@users.noreply.github.com>
2024-11-19 19:06:31 -0500

    Update Estante.cpp

--- 2024-1/labs/lab08/Estante.cpp
+++ 2024-1/labs/lab08/Estante.cpp
@@ -1,2 +1,40 @@
 //Author: MAKO
 #include "header/Estante.h"
+
+Estantes::Estantes() {
+    clase = {};
+    id = {};
+    capacidad = {};
+    Llibros = {};
+}
+
+Estantes::Estantes(const Estantes& orig) {
+}
+
+Estantes::~Estantes() {
+}
+
+void Estantes::SetCapacidad(double capacidad) {
+    this->capacidad = capacidad;
+}
+
+double Estantes::GetCapacidad() const {
+    return capacidad;
+}
+
+void Estantes::SetId(int id) {
+    this->id = id;
+}
+
+int Estantes::GetId() const {
+    return id;
+}
+
+void Estantes::SetClase(char clase) {
+    this->clase = clase;
+}
+
+char Estantes::GetClase() const {
+    return clase;
+}
+
